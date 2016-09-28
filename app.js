d705c004ab3195eb0c7c4d5b223a0d5d88eca63d
Bill42362 <bill42362@gmail.com>
2016-09-28 18:18:16 +0800

    Remove unneeded react files and add prototype of socket server.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Fs = require('fs');
 const Hapi = require('hapi');
 const Http2 = require('http2');
 const Inert = require('inert');
+const Io = require('socket.io');
 
 const App = function() {};
 App.prototype.server = new Hapi.Server();
@@ -11,7 +12,6 @@ App.prototype.listener = Http2.createServer({
     key: Fs.readFileSync('./ssl/localhost.key'),
     cert: Fs.readFileSync('./ssl/localhost.crt')
 });
-//App.prototype.listener.prototype.address = function() { return this._server.address() }
 App.prototype.routes = [
     { method: 'get', path: '/{param*}', handler: { directory: {
         path: 'dist/html/reception', redirectToSlash: true, index: ['index.html'],
@@ -20,17 +20,22 @@ App.prototype.routes = [
     { method: 'get', path: '/css/{param*}', handler: { directory: { path: 'dist/css', } } },
     { method: 'get', path: '/data/{param*}', handler: { directory: { path: 'data/', } } },
 ];
-App.prototype.run = function() {
+App.prototype.webServerDebug = Debug('WebServer');
+App.prototype.runWebServer = function() {
     const server = this.server;
     server.connection({listener: this.listener, port: '3000', tls: true});
     server.register(Inert, () => {});
     server.route(this.routes);
     server.start(err => {
-        err && Debug('http2:error')(err);
-        Debug('http2')(`Started ${server.connections.length} connections`);
-    });
+        err && this.webServerDebug(err);
+        this.webServerDebug(`Started ${server.connections.length} connections`);
+    }, this);
+};
+App.prototype.runSocketServer = function() {
+    this.io = Io.listen(5566);
 };
 module.exports = App;
 
 const app = new App();
-app.run();
+app.runWebServer();
+app.runSocketServer();
